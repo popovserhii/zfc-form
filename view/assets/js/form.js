7be5a0c007d164ea458d5134a6b63961162b7400
vladgemtyp <vlad.gem.typ@gmail.com>
2018-02-23 07:09:28 +0200

    .add-field-group script has been added

--- view/assets/js/form.js
+++ view/assets/js/form.js
@@ -15,3 +15,21 @@ $(document).on('submit', 'form.ajax', function(event) {
 	});
 	event.preventDefault();
 });
+
+$(document).on('click', '.add-field-group', function (e) {
+    var fieldset = $('#' + $(this).data('group-id'));
+    var fieldGroups = fieldset.find('.field-group');
+    var template = $(fieldset.next('span').data('template').replace(/__index__/g, fieldGroups.length));
+    var fieldGroupTemp = fieldGroups.last().clone();
+
+    fieldGroupTemp.children('.has-error').removeClass('has-error').end().find('.list-error').remove();
+    fieldGroupTemp.find('input, button, select, textarea, label').each(function () {
+        var elm = $(this);
+        var attrName = elm.is('label') ? 'for' : 'name';
+        var name = elm.attr(attrName).replace(fieldGroups.length - 1, fieldGroups.length);
+        elm.is('label') ? elm.attr(attrName, name) : elm.replaceWith(template.find('[' + attrName + '="' + name + '"]'));
+    });
+    fieldGroupTemp.appendTo(fieldset);
+
+    return false;
+});
